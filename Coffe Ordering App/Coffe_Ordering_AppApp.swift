6af063a366da11a39879b4695f86f23204aff3a3
Leon <mrtonmoy12@gmail.com>
2022-05-20 22:20:16 +0600

    Initial Commit

---
+++ Coffe Ordering App/Coffe_Ordering_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffe_Ordering_AppApp.swift
+//  Coffe Ordering App
+//
+//  Created by Mahbubur Rashid Leon on 20/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Coffe_Ordering_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffe Ordering App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coffe Ordering App
+//
+//  Created by Mahbubur Rashid Leon on 20/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
