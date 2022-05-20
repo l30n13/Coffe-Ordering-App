18020c8f59bdb0bf420f4271bad8b554eac72d73
Leon <mrtonmoy12@gmail.com>
2022-05-21 02:11:33 +0600

    -- Web service completed for create order

---
+++ Coffe Ordering App/Models/CreateOrderResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CreateOrderResponse.swift
+//  Coffe Ordering App
+//
+//  Created by Mahbubur Rashid Leon on 21/5/22.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+    let success: Bool
+}
