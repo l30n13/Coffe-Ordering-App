fe5b270df1b3151f3a892cfa72824bdd0a400e2d
Leon <mrtonmoy12@gmail.com>
2022-05-20 22:41:19 +0600

    -- Web service added to fetch the order list

---
+++ Coffe Ordering App/Services/WebService.swift
@@ -0,0 +1,29 @@
+//
+//  WebService.swift
+//  Coffe Ordering App
+//
+//  Created by Mahbubur Rashid Leon on 20/5/22.
+//
+
+import Foundation
+
+class WebService {
+    func getAllOrders(completion: @escaping ([Order]?) -> Void) {
+        guard let url = URL(string: "") else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            
+            let orders = try? JSONDecoder().decode([Order].self, from: data)
+            DispatchQueue.main.async {
+                completion(orders)
+            }
+        }.resume()
+    }
+}
